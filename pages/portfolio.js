11c3d0e24d266e99d826c16b75863d603c703359
negirupesh <negi.rupesh.4@gmail.com>
2020-12-07 12:10:13 +0530

    updates

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,22 +1,96 @@
 import React, { useEffect, useState } from 'react';
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import { Box } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 // import { useSelector } from 'react-redux';
+import { motion } from "framer-motion";
 
-const style = {
-    backdrop: {
-        zIndex: 5,
-        color: '#fff',
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
     },
-};
+    gridList: {
+        width: 500,
+        height: 450,
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+            'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    icon: {
+        color: 'white',
+    },
+}));
 
-const useStyles = makeStyles(style);
 
 const Portfolio = ({ props, dispatch }) => {
     const classes = useStyles();
     return <>
-        <Box>Portfolio</Box>
+        <Box>
+            <div className="container technologies-section">
+                <h2 className="text-center " data-wow-duration="2s" data-wow-delay="0.6s"> Work</h2>
+
+                <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                        <div className="col-md-5 col-sm-6 wow fadeInUp animated animated" data-wow-duration="2s" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDuration: '2s', animationDelay: '0.6s', animationName: 'fadeInUp' }}>
+                            <div className="work_box work_box_margan">
+                                <img className="img-fluid" src="img/work2.jpg" />
+                                <div className="work_box_in">
+                                    UX Conception<br />
+                                                        Interface Design<br />
+                                                            Web Frontend Development
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+
+                <div className="row justify-content-md-center">
+
+                    
+                    <div className="col-md-1"> </div>
+                    <div className="col-md-5 col-sm-6 wow fadeInUp animated animated" data-wow-duration="2s" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDuration: '2s', animationDelay: '0.6s', animationName: 'fadeInUp' }}>
+                        <div className="work_box work_box_margan">
+                            <img className="img-fluid" src="img/work2.jpg" />
+                            <div className="work_box_in">
+                                UX Conception<br />
+                                                        Interface Design<br />
+                                                            Web Frontend Development
+                                </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-5 col-sm-6 wow fadeInUp animated animated" data-wow-duration="2s" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDuration: '2s', animationDelay: '0.6s', animationName: 'fadeInUp' }}>
+                        <div className="work_box">
+                            <img className="img-fluid" src="img/work2.jpg" />
+                            <div className="work_box_in">
+                                UX Conception<br />
+                                                                    Interface Design<br />
+                                                                        Web Frontend Development
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col-md-1"> </div>
+                    <div className="col-md-5 col-sm-6 wow fadeInUp animated animated" data-wow-duration="2s" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDuration: '2s', animationDelay: '0.6s', animationName: 'fadeInUp' }}>
+                        <div className="work_box work_box_margan">
+                            <img className="img-fluid" src="img/work1.jpg" />
+                            <div className="work_box_in">
+                                UX Conception<br />
+                                                                                Interface Design<br />
+                                                                                    Web Frontend Development
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Box>
     </>;
 }
 
--- pages/technology.js
+++ pages/technology.js
@@ -17,45 +17,45 @@ const Technology = ({ props, dispatch }) => {
     const classes = useStyles();
     return <>
         <Box>
-        <div className="container technologies-section">
-<h2 className="text-center " data-wow-duration="2s" data-wow-delay="0.6s"> Technologies
+            <div className="container technologies-section">
+                <h2 className="text-center " data-wow-duration="2s" data-wow-delay="0.6s"> Technologies
 </h2>
-<div className="row coloumn-5 align-items-center ">
-<div className="col-md-3 col-sm-4 ">
-<div className="technologies_box">
-<img src="img/angular.png"/>
-<h5>Angular</h5>
-</div>
-</div>
-<div className="col-md-3 col-sm-4 ">
-<div className="technologies_box">
-<img src="img/laravel.png"/>
-<h5>Laravel</h5>
-</div>
-</div>
-<div className="col-md-3 col-sm-4 ">
-<div className="technologies_box">
-<img src="img/node.png"/>
-<h5>Node Js</h5>
-</div>
-</div>
-<div className="col-md-3 col-sm-4 ">
-<div className="technologies_box">
-<img src="img/react.png"/>
-<h5>React Js</h5>
-</div>
-</div>
-<div className="col-md-3 col-sm-4 ">
-<div className="technologies_box">
-<img src="img/react_native.png"/>
-<h5 >React Native</h5>
-</div>
-</div>
+                <div className="row coloumn-5 align-items-center ">
+                    <div className="col-md-3 col-sm-4 ">
+                        <div className="technologies_box">
+                            <img src="img/angular.png" />
+                            <h5>Angular</h5>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-4 ">
+                        <div className="technologies_box">
+                            <img src="img/laravel.png" />
+                            <h5>Laravel</h5>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-4 ">
+                        <div className="technologies_box">
+                            <img src="img/node.png" />
+                            <h5>Node Js</h5>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-4 ">
+                        <div className="technologies_box">
+                            <img src="img/react.png" />
+                            <h5>React Js</h5>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-4 ">
+                        <div className="technologies_box">
+                            <img src="img/react_native.png" />
+                            <h5 >React Native</h5>
+                        </div>
+                    </div>
 
 
 
-</div>
-</div>
+                </div>
+            </div>
 
         </Box>
     </>;
